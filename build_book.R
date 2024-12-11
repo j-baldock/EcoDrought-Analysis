677cfaff287dd247c3a914859a40aaa19b30e8af
j-baldock <jbaldock@usgs.gov>
2024-12-11 12:48:32 -0500

    add slope-int correlation and variance components

--- build_book.R
+++ build_book.R
@@ -1,6 +1,8 @@
 library(quarto)
 library(knitr)
 
+# To add a chapter to the book, or shuffle the order of chapters, first render the single chapter, then render the entire book (after updating the .yml file)
+
 # Render all
 quarto::quarto_render(output_format = "html")
 
