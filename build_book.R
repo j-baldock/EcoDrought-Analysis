602161d50d13e136bfba162eecf085c08c6404dd
j-baldock <jbaldock@usgs.gov>
2025-01-29 09:17:42 -0700

    render GgDiffJAGS, GgDiffKS, GgStoryPlots

--- build_book.R
+++ build_book.R
@@ -4,7 +4,7 @@ library(knitr)
 # To add a chapter to the book, or shuffle the order of chapters, first render the single chapter, then render the entire book (after updating the .yml file)
 
 # Render all
-quarto::quarto_render(output_format = "html")
+quarto::quarto_render(output_format = "html", cache_refresh = TRUE)
 
 # Render single chapter only
 quarto::quarto_render("index.qmd", 
