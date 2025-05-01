42935b7cb6c5972841ba78ef6b2226c072333d62
j-baldock <jbaldock@usgs.gov>
2025-05-01 15:23:13 -0600

    add low flow to build

--- build_book.R
+++ build_book.R
@@ -51,6 +51,10 @@ quarto::quarto_render("Covariates/PASTA.qmd",
                       cache_refresh = TRUE, # default is FALSE
                       output_format = "html")
 
+quarto::quarto_render("Qualitative/SpaceTimeVar.qmd", 
+                      #cache_refresh = TRUE, # default is FALSE
+                      output_format = "html")
+
 quarto::quarto_render("Qualitative/Hysteresis.qmd", 
                       #cache_refresh = TRUE, # default is FALSE
                       output_format = "html")
